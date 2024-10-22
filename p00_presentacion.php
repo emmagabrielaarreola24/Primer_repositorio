d21e565f7ec65702da70d48928565ce05e23ef65
emmagabrielaarreola24 <aeve080324mdgrlma4@cbtis130.edu.mx>
2024-10-22 08:50:07 -0600

    Add files via upload

---
+++ p00_presentacion.php
@@ -0,0 +1,15 @@
+<?php
+/* cbtis 89
+programa que 
+emma gabriela arreola vallejo
+3A programacion matutino
+*/
+
+echo "mi nombre es : emma gabriela arreola vallejo";
+echo "<br>";
+echo "estudio en el CBTIS 89";
+echo "<br>";
+echo "estoy estudiando la carrera deprogramacion 3A matutino";
+echo "<br>";
+echo "vivo en faccionamiento ex hacienda de tapias, santa elisa";
+?>
