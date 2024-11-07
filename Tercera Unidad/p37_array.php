78e61f3ab843d0a512eff4cba4559d8cd6f436b6
emmagabrielaarreola24 <aeve080324mdgrlma4@cbtis130.edu.mx>
2024-11-07 09:00:56 -0600

    Delete Tercera Unidad/p37_array.php

--- Tercera Unidad/p37_array.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*cbtis 89
-programa que almacena  datos en un arreglo y posteriormente losimprime por medio de un ciclo for 
-emma gabriela arreola vallejo
-3 A programacion matutino 
-*/
-
-arraynumrico(1,-2,3-6,)
